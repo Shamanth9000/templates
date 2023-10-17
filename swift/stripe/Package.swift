ee6bf63ccc0ace7543a5fbad568d20ac6af6fa1b
Shamanth <shamanth1604@gmail.com>
2023-10-17 21:09:38 +0530

    kjn

--- swift/stripe/Package.swift
+++ swift/stripe/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "swift-function",
     dependencies: [
-        .package(url: "git@github.com:appwrite/sdk-for-swift.git", from: "4.0.1"),
+        .package(url: "https://github.com/appwrite/sdk-for-swift.git", from: "4.0.1"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
         .package(url: "https://github.com/vapor-community/stripe-kit.git", from: "22.0.0")
     ],
